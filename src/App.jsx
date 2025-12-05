af12103dbfe2fd404d3e5d57f9b8db677d5baa4c
manish2kumar <kr.manish.0223@gmail.com>
2025-12-05 12:34:49 +0530

    Add GitHub Actions deploy workflow

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>React + CI/CD + AWS EC2</h1>
+      <h1>React + CI/CD + AWS EC2 Working live</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
